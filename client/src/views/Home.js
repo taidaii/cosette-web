1c15eab0a6aaa49ca9a66f24af491d629089fa4f
jarretttai <jarrett0699@outlook.com>
2022-04-05 23:17:33 -0700

    corrected some code indentation

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -26,81 +26,78 @@ export default function () {
           <Tab eventKey="student" title="Student's View">
           </Tab>
         </Tabs>
-            <Row className='btn-row'>
-              <Col>
-                <Stack direction="horizontal" gap={2}>
-                  <Dropdown>
-                    <Dropdown.Toggle id="dropdown-basic" variant="outline-primary">
-                      Template
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                      <Dropdown.Item href="#/action-1">Hello World</Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                
-                  <Button variant="outline-primary">
-                    Upload
-                  </Button>
-                
-                  <Button variant="link">
-                    Manage
-                  </Button>
-                </Stack>
-              </Col>
-              <Col>
-                <Button variant="success" onClick={!isLoading ? handleClick : null}>
-                  Verify
-                </Button>
-              </Col>
-            </Row>
-            <Row>
-              <Col style={{width: "20%"}}>
-                <Stack gap={4}>
-                  <CodeMirror
-                    className="bg-light border"
-                    value="CREATE TABLE hello_world {};"
-                    height={tabKey == 'ta' ? "190px" : "298px"}
-                    extensions={[sql({})]}
-                    onChange={(value, viewUpdate) => {
-                      console.log('value:', value);
-                    }}
-                  />
-                  {
-                    tabKey == 'ta' ?
-                    <CodeMirror
-                    className="bg-light border"
-                      value="SELECT * FROM hello_world;"
-                      height="190px"
-                      extensions={[sql({})]}
-                      onChange={(value, viewUpdate) => {
-                        console.log('value:', value);
-                      }}
-                    />
-                    : null
-                  }
-                  <CodeMirror
-                    className="bg-light border"
-                    value="SELECT * FROM hello_world;"
-                    height={tabKey == 'ta' ? "190px" : "298px"}
-                    extensions={[sql({})]}
-                    onChange={(value, viewUpdate) => {
-                      console.log('value:', value);
-                    }}
-                  />
-                </Stack>
-              </Col>
-              <Col>
-                <div className='result'>
-                  <h3 style={{}}>Result</h3>
-                  {isLoading ? <div id="feedback">Two queries are equivalent!</div> : null}
-                </div>
-              </Col>
-            </Row>
-          
 
-          
+        <Row className='btn-row'>
+          <Col>
+            <Stack direction="horizontal" gap={2}>
+              <Dropdown>
+                <Dropdown.Toggle id="dropdown-basic" variant="outline-primary">
+                  Template
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Hello World</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            
+              <Button variant="outline-primary">
+                Upload
+              </Button>
+            
+              <Button variant="link">
+                Manage
+              </Button>
+            </Stack>
+          </Col>
+          <Col>
+            <Button variant="success" onClick={!isLoading ? handleClick : null}>
+              Verify
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col style={{width: "20%"}}>
+            <Stack gap={4}>
+              <CodeMirror
+                className="bg-light border"
+                value="CREATE TABLE hello_world {};"
+                height={tabKey == 'ta' ? "190px" : "298px"}
+                extensions={[sql({})]}
+                onChange={(value, viewUpdate) => {
+                  console.log('value:', value);
+                }}
+              />
+              {
+                tabKey == 'ta' ?
+                <CodeMirror
+                className="bg-light border"
+                  value="SELECT * FROM hello_world;"
+                  height="190px"
+                  extensions={[sql({})]}
+                  onChange={(value, viewUpdate) => {
+                    console.log('value:', value);
+                  }}
+                />
+                : null
+              }
+              <CodeMirror
+                className="bg-light border"
+                value="SELECT * FROM hello_world;"
+                height={tabKey == 'ta' ? "190px" : "298px"}
+                extensions={[sql({})]}
+                onChange={(value, viewUpdate) => {
+                  console.log('value:', value);
+                }}
+              />
+            </Stack>
+          </Col>
+          <Col>
+            <div className='result'>
+              <h3 style={{}}>Result</h3>
+              {isLoading ? <div id="feedback">Two queries are equivalent!</div> : null}
+            </div>
+          </Col>
+        </Row>
 
-        
       </Container>         
     </>
   );
