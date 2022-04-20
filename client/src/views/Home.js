0aff4d31b8d1a611465fc2e64c5d3bac99f577cc
Louis5499 <louisby0123@gmail.com>
2022-04-19 21:27:02 -0700

    add files

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-anonymous-default-export */
 import { Container, Row, Col, Dropdown, Stack, Spinner, Table, Tab, Tabs } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button'
 import CodeMirror from '@uiw/react-codemirror';
@@ -13,20 +12,18 @@ export default function () {
 
   const [isLoading, setLoading] = useState(false);
   const [tabKey, setTabKey] = useState('ta');
-  const [createTableStatement, setCreateTableStatement] = useState("CREATE TABLE indiv_sample_nyc(\n cmte_id INT,\n transaction_amt INT,\n name VARCHAR(10),\n str_name VARCHAR(10)\n );");
+  const [createTableStatement, setCreateTableStatement] = useState("CREATE TABLE indiv_sample_nyc(\n cmte_id INT,\n transaction_amt INT,\n name VARCHAR(10),\n str_name VARCHAR(10)\n );\n\nCREATE TABLE indiv_sample_nyc2(\n cmte_idt INT,\n transaction_amtt INT,\n namet VARCHAR(10),\n str_namet VARCHAR(10)\n );");
   const [taSubmission, setTaSubmission] = useState("SELECT * FROM indiv_sample_nyc WHERE cmte_id = 1 AND name LIKE '%test%' AND str_name LIKE '%test3%';");
   const [studentSubmission, setStudentSubmission] = useState("SELECT * FROM indiv_sample_nyc WHERE cmte_id = 2 AND name LIKE '%test%' AND str_name LIKE '%test3%';");
   const [resultStatus, setResultStatus] = useState("");
   const [resultLog, setResultLog] = useState("");
-  const [resultTableCols, setResultTableCols] = useState("");
-  const [resultTableRows, setResultTableRows] = useState("");
+  const [resultTables, setResultTables] = useState([]);
 
   const handleClick = async() => {
     setLoading(true);
     setResultStatus("");
     setResultLog("");
-    setResultTableCols("");
-    setResultTableRows("");
+    setResultTables([]);
     const result = await axios.post('http://localhost:5000/check', {
       // schema: "CREATE TABLE indiv_sample_nyc(\n cmte_id INT,\n transaction_amt INT,\n name VARCHAR(10),\n str_name VARCHAR(10)\n );",
       schema: createTableStatement,
@@ -34,32 +31,37 @@ export default function () {
       student_input: studentSubmission
     });
     console.log(result);
-    const { status, test_log } = result.data;
+    let { status, test_log } = result.data;
+    test_log = test_log.replaceAll("<", "&lt").replaceAll(">", "&gt").replace(/\n/g, "<br />");
     setResultStatus(status);
-    setResultLog(test_log);
     if (test_log.indexOf("Table") !== -1) {
       // "(Table \"INDIV_SAMPLE_NYC\" '(\"CMTE_ID\" \"TRANSACTION_AMT\" \"NAME\" \"STR_NAME\") '(((1 0 \"0\" \"0\") . 1)))↵"
-      const tblName = test_log.substring(test_log.indexOf("Table")+7, test_log.indexOf("\"", test_log.indexOf("Table \"") + 7));
-      console.log(tblName);
-      const colNames = test_log.substring(test_log.indexOf("(", 1)+1, test_log.indexOf(")", 1)).replaceAll("\"", "").split(" ");
-      let tblCols = "<th>#</th>";
-      for (let name of colNames) tblCols += "<th>" + name + "</th>";
-      setResultTableCols(tblCols);
-      console.log(colNames);
-      const recordsPerRow = test_log.substring(test_log.indexOf("(((")+3, test_log.indexOf(")", test_log.indexOf("((("))).split(" ");
-      console.log(recordsPerRow);
-      const recordNumPerRow = test_log.substring(test_log.indexOf(".", test_log.indexOf("((("))+2, test_log.indexOf(")))"));
-      // TODO: Modify code if there're multiple different kinds of record
-      for (let i=0; i<recordNumPerRow; i++) {
-        let tblHtml = "<tr><td>" + i + "</td>";
-        for (let record of recordsPerRow) tblHtml += "<td>" + record + "</td>";
-        tblHtml += "</tr>";
-        console.log(tblHtml);
-        setResultTableRows(tblHtml);
+      const tablesTestLogs = test_log.split("<br />");
+      const generatedTables = [];
+      for (const tablesTestLog of tablesTestLogs) {
+        if (tablesTestLog.length <= 0) continue;
+        const resultTable = { cols: "", rows: "" };
+        const tblName = tablesTestLog.substring(tablesTestLog.indexOf("Table")+7, tablesTestLog.indexOf("\"", tablesTestLog.indexOf("Table \"") + 7));
+        // Find all cols
+        const colNames = tablesTestLog.substring(tablesTestLog.indexOf("(", 1)+1, tablesTestLog.indexOf(")", 1)).replaceAll("\"", "").split(" ");
+        let tblCols = "<th>#</th>";
+        for (let name of colNames) tblCols += "<th>" + name + "</th>";
+        resultTable["cols"] = tblCols;
+        // Find all records
+        if (tablesTestLog.indexOf("(((") !== -1) {
+          const recordsPerRow = tablesTestLog.substring(tablesTestLog.indexOf("(((")+3, tablesTestLog.indexOf(")", tablesTestLog.indexOf("((("))).split(" ");
+          const recordNumPerRow = test_log.substring(test_log.indexOf(".", test_log.indexOf("((("))+2, test_log.indexOf(")))"));
+          // TODO: Modify code if there're multiple different kinds of record
+          for (let i=0; i<recordNumPerRow; i++) {
+            let tblHtml = "<tr><td>" + i + "</td>";
+            for (let record of recordsPerRow) tblHtml += "<td>" + record + "</td>";
+            tblHtml += "</tr>";
+            resultTable["rows"] = tblHtml;
+          }
+        }
+        generatedTables.push(resultTable);
       }
-      console.log(recordNumPerRow);
-      console.log(resultTableCols);
-      console.log(resultTableRows);
+      setResultTables(generatedTables);
     }
     setLoading(false);
   }
@@ -156,9 +158,13 @@ export default function () {
               { (resultStatus === "Undecidable") && <Button variant="secondary" active>Not Decidable</Button> }
               <br></br>
               <br></br>
-              { (resultTableRows !== "" && <p>The following table is the counterexample</p>)}
-              { (resultTableRows !== "" && <Table striped bordered hover><thead><tr>{ReactHtmlParser(resultTableCols)}</tr></thead><tbody>{ReactHtmlParser(resultTableRows)}</tbody></Table>)}
-              { (resultTableRows === "" && resultLog) }
+              { (resultTables.length > 0  && <p>The following table is the counterexample</p>)}
+              { (resultTables.length > 0  && 
+                resultTables.map((item, idx) => {
+                  return <Table striped bordered hover key={idx}><thead><tr>{ReactHtmlParser(item.cols)}</tr></thead><tbody>{ReactHtmlParser(item.rows)}</tbody></Table>
+                })
+                )}
+              { (resultTables.length === 0 && ReactHtmlParser(resultLog)) }
             </div>
           </Col>
         </Row>
